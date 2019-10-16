4fd055114d990f6fa6efd2b11cfc0afebf6aac35
Gavinconfession <wsx1128@outlook.com>
2019-10-16 10:55:07 +0800

    Initial commit

---
+++ src/com/wsx/jdbc_learning/jdbc_learning.java
@@ -0,0 +1,24 @@
+package com.wsx.jdbc_learning;
+
+import com.mysql.cj.jdbc.Driver;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class jdbc_learning {
+
+    @Test
+    public void testConnect() throws SQLException {
+        Driver driver = new com.mysql.cj.jdbc.Driver();
+
+        DriverManager.registerDriver(driver);
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "w19971128");
+
+        System.out.println(connection);
+    }
+
+}
