b05441a681dfc446e5bad402d960ae00b4a4056e
Gavinconfession <wsx1128@outlook.com>
2019-11-02 20:41:38 +0800

    some change

---
+++ Easy_test_Tomcat/src/com/wsx/web/Servlet/HttpDemo1.java
@@ -0,0 +1,23 @@
+package com.wsx.web.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/HttpDemo1")
+public class HttpDemo1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do get");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do post...");
+        System.out.println("asdaffasdfasdfa");
+    }
+}
